6fc944665bfc6c019578766b0e24e80f4c92d71c
Arthur Murillo <arthurmurillo9@gmail.com>
2019-11-18 22:00:25 +0100

    Modification contrôleur et vue
    Modification de la route menant à la page d'accueil, modification de la vue de la page d'accueil pour afficher un message simple

--- src/Controller/NoteoController.php
+++ src/Controller/NoteoController.php
@@ -8,12 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class NoteoController extends AbstractController
 {
     /**
-     * @Route("/noteo", name="noteo")
+     * @Route("/", name="Noteo_accueil")
      */
     public function index()
     {
-        return $this->render('noteo/index.html.twig', [
-            'controller_name' => 'NoteoController',
-        ]);
+        return $this->render('noteo/index.html.twig'
+        );
     }
 }
