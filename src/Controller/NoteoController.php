828dd301edbbd25d70ac2ed09fe37040096d92f2
Dylan Mendiboure <d.mendiboure64@gmail.com>
2020-01-14 19:57:10 +0100

    Création d'une page listant l'ensemble des logos

--- src/Controller/NoteoController.php
+++ src/Controller/NoteoController.php
@@ -14,4 +14,12 @@ class NoteoController extends AbstractController
     {
         return $this->render('noteo/login.html.twig');
     }
+
+    /**
+     * @Route("/icones", name="noteo_icones")
+     */
+    public function icones()
+    {
+        return $this->render('noteo/icones.html.twig');
+    }
 }
