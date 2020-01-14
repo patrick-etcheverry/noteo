adec638e3b031def0d288231a81e84f79fcaba60
Arthur Murillo <arthurmurillo9@gmail.com>
2020-01-14 15:17:47 +0100

    Ajout des champs
    Modification du formulaire de création d'un groupe pour inclure un champ de saisie d'un fichier CSV qui pourra être analysé. Ajout d'un champ de selection d'un objet professeur pour éviter un bug avant de mettre en place l'authentification

--- src/Form/GroupeEtudiantType.php
+++ src/Form/GroupeEtudiantType.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\GroupeEtudiant;
+use App\Entity\Enseignant;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class GroupeEtudiantType extends AbstractType
 {
@@ -15,6 +18,8 @@ class GroupeEtudiantType extends AbstractType
             ->add('nom')
             ->add('description')
             ->add('estEvaluable')
+            ->add('enseignant', EntityType::class, ['class' => Enseignant::class, 'choice_label' => 'nom'])
+            ->add('fichier', FileType::class, ['mapped' => false])
         ;
     }
 
