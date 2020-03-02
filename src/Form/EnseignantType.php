5be97489cdfe15e892c0747e49ded4298242d5cf
Dylan Mendiboure <d.mendiboure64@gmail.com>
2020-03-02 22:40:11 +0100

    Adaptation du formulaire enseignant, ajout de la double saisie du mot de passe

--- src/Form/EnseignantType.php
+++ src/Form/EnseignantType.php
@@ -6,6 +6,8 @@ use App\Entity\Enseignant;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class EnseignantType extends AbstractType
 {
@@ -15,7 +17,14 @@ class EnseignantType extends AbstractType
             ->add('nom')
             ->add('prenom')
             ->add('email')
-            ->add('password')
+            ->add('password',RepeatedType::class, [
+      'type' => PasswordType::class,
+      'invalid_message' => 'Les mots de passe saisis ne correspondent pas.',
+      'options' => ['attr' => ['class' => 'password-field']],
+      'required' => true,
+      'first_options'  => ['label' => 'Saisissez le une première fois...'],
+      'second_options' => ['label' => 'Puis une deuxième fois...'],
+    ])
 
         ;
     }
