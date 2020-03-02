47c54b403bad5457ca37bfbe9e6f741331586909
Dylan Mendiboure <d.mendiboure64@gmail.com>
2020-03-02 21:18:46 +0100

    Correction suite à erreur dans le nom Authenticator

--- src/Security/EnseignantAuthenticator.php
+++ src/Security/EnseignantAuthenticator.php
@@ -19,7 +19,7 @@ use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
-class EnseignantAuthentictcatorAuthenticator extends AbstractFormLoginAuthenticator
+class EnseignantAuthenticator extends AbstractFormLoginAuthenticator
 {
     use TargetPathTrait;
 
