f04126dada988747639922e4fa0379b78ffb51dc
Dylan Mendiboure <d.mendiboure64@gmail.com>
2020-03-04 01:22:42 +0100

    Configuration et test du serveur de mail

--- src/Controller/NoteoController.php
+++ src/Controller/NoteoController.php
@@ -7,19 +7,25 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class NoteoController extends AbstractController
 {
-    /**
-     * @Route("/", name="noteo_login")
-     */
-    public function index()
-    {
-        return $this->redirectToRoute('app_login');
-    }
+  /**
+  * @Route("/", name="noteo_login")
+  */
+  public function index()
+  {
+    return $this->redirectToRoute('app_login');
+  }
 
-    /**
-     * @Route("/icones", name="noteo_icones")
-     */
-    public function icones()
-    {
-        return $this->render('noteo/icones.html.twig');
-    }
+  /**
+  * @Route("/icones", name="noteo_icones")
+  */
+  public function icones(\Swift_Mailer $mailer)
+  {
+    $message = (new \Swift_Message('Hello Email'))
+    ->setFrom('contact@noteo.me')
+    ->setTo('d.mendiboure64@gmail.com')
+    ->setBody('You should see me from the profiler!');
+
+    $mailer->send($message);
+    return $this->render('noteo/icones.html.twig');
+  }
 }
