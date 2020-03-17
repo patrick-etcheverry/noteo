16e2025f3bf75b5f58af9dda89f5aba547329d59
Arthur Murillo <arthurmurillo9@gmail.com>
2020-03-17 22:11:47 +0100

    Ajout de nouvelles règles de validation sur Enseignant

--- src/Entity/Enseignant.php
+++ src/Entity/Enseignant.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\EnseignantRepository")
@@ -22,6 +23,7 @@ class Enseignant implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Assert\Email
      */
     private $email;
 
@@ -38,11 +40,13 @@ class Enseignant implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(max=255)
      */
     private $nom;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(max=255)
      */
     private $prenom;
 
