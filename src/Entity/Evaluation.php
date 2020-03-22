4b05bda832dc3ff40d9288c08eaf99386e0a72d6
Arthur Murillo <arthurmurillo9@gmail.com>
2020-03-22 22:01:38 +0100

    Suppression de méthodes et actions inutiles

--- src/Entity/Evaluation.php
+++ src/Entity/Evaluation.php
@@ -67,33 +67,6 @@ class Evaluation
         return $this;
     }
 
-    public function slugify($text)
-    {
-        // replace non letter or digits by -
-        $text = preg_replace('~[^\pL\d]+~u', '-', $text);
-
-        // transliterate
-        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-
-        // remove unwanted characters
-        $text = preg_replace('~[^-\w]+~', '', $text);
-
-        // trim
-        $text = trim($text, '-');
-
-        // remove duplicate -
-        $text = preg_replace('~-+~', '-', $text);
-
-        // lowercase
-        $text = strtolower($text);
-
-        if (empty($text)) {
-            return 'n-a';
-        }
-
-        return $text;
-    }
-
     public function __construct()
     {
         $this->parties = new ArrayCollection();
