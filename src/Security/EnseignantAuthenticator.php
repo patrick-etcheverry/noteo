146eb4ac3331d9f66f449ac8311d5e658566798a
Dylan Mendiboure <d.mendiboure64@gmail.com>
2020-03-23 13:37:59 +0100

    Traduction du message d'erreur quand l'adresse mail n'existe pas

--- src/Security/EnseignantAuthenticator.php
+++ src/Security/EnseignantAuthenticator.php
@@ -68,7 +68,7 @@ class EnseignantAuthenticator extends AbstractFormLoginAuthenticator
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Email could not be found.');
+            throw new CustomUserMessageAuthenticationException('Adresse mail introuvable.');
         }
 
         return $user;
