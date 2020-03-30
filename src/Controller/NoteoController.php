0db00c83ba08e5686186dd4affbfd91d8b2f4261
Arthur Murillo <arthurmurillo9@gmail.com>
2020-03-30 14:11:40 +0200

    Suppression de la page des icônes

--- src/Controller/NoteoController.php
+++ src/Controller/NoteoController.php
@@ -14,18 +14,4 @@ class NoteoController extends AbstractController
   {
     return $this->redirectToRoute('app_login');
   }
-
-  /**
-  * @Route("/icones", name="noteo_icones")
-  */
-  public function icones(\Swift_Mailer $mailer)
-  {
-    $message = (new \Swift_Message('Hello Email'))
-    ->setFrom('contact@noteo.me')
-    ->setTo('d.mendiboure64@gmail.com')
-    ->setBody('You should see me from the profiler!');
-
-    $mailer->send($message);
-    return $this->render('noteo/icones.html.twig');
-  }
 }
