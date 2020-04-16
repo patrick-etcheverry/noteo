34d330ad6e19a9e2dfef3d9366fc029dc5ea08a2
Arthur Murillo <arthurmurillo9@gmail.com>
2020-04-16 19:07:11 +0200

    Ajout du dossier sql nécéssaire pour l'installation dans le projet

---
+++ manuels/noteo_admin_base.sql
@@ -0,0 +1,2 @@
+INSERT INTO `enseignant` (`id`, `email`, `roles`, `password`, `nom`, `prenom`)
+VALUES (1, 'noteo@admin.fr', '[\"ROLE_USER\",\"ROLE_ADMIN\"]', '$2y$10$DzfLbLyjpDUiLyYlPcw.L.RqtdOxxS7XCuXeg.bq3Glu5gG9W04WO', 'Noteo', 'Admin');
