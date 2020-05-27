18b745a0b6f352ee48afe900a2d4cc51ee1d4464
Arthur Murillo <arthurmurillo9@gmail.com>
2020-05-27 13:53:35 +0200

    Ajout de la fonctionnalité pour préparer l'application pour année suiv.
    Ajout d'une fonctionnalité pour pouvoir vider la base de données (sauf le profil des enseignants) à chaque nouvelle année universitaire

--- src/Controller/NoteoController.php
+++ src/Controller/NoteoController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\EtudiantRepository;
+use App\Repository\GroupeEtudiantRepository;
+use App\Repository\StatutRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,4 +17,42 @@ class NoteoController extends AbstractController
   {
     return $this->redirectToRoute('app_login');
   }
+
+    /**
+     * @Route("/options", name="noteo_options")
+     */
+    public function optionsApplication()
+    {
+        $this->denyAccessUnlessGranted('OPTIONS_APPLICATION', $this->getUser());
+        return $this->render("app/optionsApplication.html.twig");
+
+    }
+
+    /**
+     * @Route("/preparation-annee-suivante", name="preparer_annee_suivante")
+     */
+    public function preparerAnneeSuivante(GroupeEtudiantRepository $repoGroupe, StatutRepository $repoStatut, EtudiantRepository $repoEtudiant)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        foreach ($repoGroupe->findAll() as $groupe) {
+            foreach ($groupe->getEvaluations() as $evaluation) {
+                foreach ($evaluation->getParties() as $partie) {
+                    foreach ($partie->getNotes() as $note) {
+                        $entityManager->remove($note);
+                    }
+                    $entityManager->remove($partie);
+                }
+                $entityManager->remove($evaluation);
+            }
+            $entityManager->remove($groupe);
+        }
+        foreach ($repoStatut->findAll() as $statut) {
+            $entityManager->remove($statut);
+        }
+        foreach ($repoEtudiant->findAll() as $etudiant) {
+            $entityManager->remove($etudiant);
+        }
+        $entityManager->flush();
+        return $this->redirectToRoute("noteo_options");
+    }
 }
