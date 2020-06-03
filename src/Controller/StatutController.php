68fdb9ccc925d4191c352194243d2baa69323d98
Arthur Murillo <arthurmurillo9@gmail.com>
2020-06-03 10:22:13 +0200

    Autre correction sur statuts

--- src/Controller/StatutController.php
+++ src/Controller/StatutController.php
@@ -24,7 +24,7 @@ class StatutController extends AbstractController
     {
 
         return $this->render('statut/index.html.twig', [
-            'statuts' => $statutRepository->findAllWithStudents(),
+            'statuts' => $statutRepository->findAll(),
         ]);
     }
 
